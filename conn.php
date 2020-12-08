98187143b477fbd45cd320f807930d975a78d924
aman669 <75686441+aman669@users.noreply.github.com>
2020-12-08 21:41:27 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,3 +1,3 @@
 <?php 
-	$con = mysqli_connect('localhost','root','','studentdb');
+	$con = mysqli_connect('studentdb.cmwxgsxpwrcr.us-east-1.rds.amazonaws.com','admin','mypassword','studentdb');
  ?>
